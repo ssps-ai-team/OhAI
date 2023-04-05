450a8d0e64938d410ec583331bd37ddca7895036
Joshua Thomas Remington <106017903+ThatBlokeJosh@users.noreply.github.com>
2023-04-05 20:34:18 +0200

    Seperated chat functions to seperate file

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import openai, os;
-
-openai.api_key_path = ".env"
-model_engine = "gpt-3.5-turbo-0301"
-
-
-def chat_query(prompt):
-    completions = openai.ChatCompletion.create(
-        model=model_engine,
-        messages=[{"role": "user", "content": prompt}],
-        max_tokens=2048,
-        n=1,
-        temperature=0.5,
-    )
-
-    message = completions.choices[0].message.content
-    return message
-
-def conversaton_handler(prompt):
-    response = chat_query(prompt)
-    return response
-
-
+from chat import conversaton_handler
 from ohbot import *
 
 lang = input("What language do you want to use: ")
