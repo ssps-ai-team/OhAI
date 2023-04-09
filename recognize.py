ef33bf75245fb81bbea20908e88cc5f8b3597c9e
ThatBlokeJosh <joshtomrem@protonmail.com>
2023-04-09 21:43:10 +0200

    Improved voice activation

--- recognize.py
+++ recognize.py
@@ -1,16 +1,30 @@
 import pyttsx3
 import speech_recognition as sr
+from ohbot import *
 
 def recognize():
+    
     recognizer = sr.Recognizer()
     recognizer.energy_threshold = 32504.85796527251
     recognizer.dynamic_energy_threshold = True
     with sr.Microphone() as mic:
-        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
         audio = recognizer.listen(mic)
 
         text = recognizer.recognize_google(audio)
         text = text.lower()
 
-        return text
+    if text == "hello":
+        say("Please talk now", "en")
+        wait(1)
+        recognizer = sr.Recognizer()
+        recognizer.energy_threshold = 32504.85796527251
+        recognizer.dynamic_energy_threshold = True
+        with sr.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
 
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+
+            print(text)
+            return text
