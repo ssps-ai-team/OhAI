2a8cfc0030c9ccf16e012bffe55ae70bdb754440
ThatBlokeJosh <joshtomrem@protonmail.com>
2023-04-10 20:03:08 +0200

    Added a name for the speech recognition and improved on it

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ import sys
 def main():
     lang = sys.argv[-1] # Get the language
     try:
-        prompt = recognize()
+        if lang == "cs":
+            prompt = recognize("Davide", lang)
+            prompt += " Řekni odpověď v Češtině."
+        elif lang == "en":
+            prompt = recognize("David", lang)
+            prompt += " Say the answer in english."
+        print(prompt)
         answer = conversaton_handler(prompt) # Generate the response from the user
         print(answer) # Print the answer for debuging lol
         say(answer, lang) # Make the ohbot talk
-    except:
-        say("Can you repeat that?", lang)
+    except Exception as e:
+        print(e)
+        if lang == "cs":
+            say("Zopakujte to prosím", lang)
+        elif lang == "en":
+            say("Can you repeat that", lang)
 
 if __name__ == "__main__":
     main()
--- recognize.py
+++ recognize.py
@@ -2,33 +2,16 @@ import speech_recognition as sr
 from ohbot import *
 
 
-def recognize():
-    for device_index in sr.Microphone().list_working_microphones():
-        m = sr.Microphone(device_index=device_index)
-        print(device_index)
-        break
-    else:
-        print("No working microphones found!")
+def recognize(name, lang):
     recognizer = sr.Recognizer()
-    recognizer.energy_threshold = 32504.85796527251
+    recognizer.energy_threshold = 30604.142478090707
     recognizer.dynamic_energy_threshold = True
     with sr.Microphone() as mic:
         audio = recognizer.listen(mic)
-        text = recognizer.recognize_google(audio)
+        text = recognizer.recognize_google(audio, language=lang)
         text = text.lower()
-
-    if text == "hello":
-        say("Please talk now", "en")
-        wait(1)
-        recognizer = sr.Recognizer()
-        recognizer.energy_threshold = 32504.85796527251
-        recognizer.dynamic_energy_threshold = True
-        with sr.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
-
-            text = recognizer.recognize_google(audio)
-            text = text.lower()
-
-            print(text)
-            return text
+    
+    if text.startswith(name.lower()):
+        text = text.replace(f"{name.lower()} ", '')
+        print(text)
+        return text
